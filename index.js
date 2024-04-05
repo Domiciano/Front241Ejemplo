098a3b6d2570330c5276b90f87fec0106a9e672b
Samub96 <samuel.d.barona@gmai.com>
2024-04-05 13:01:52 -0500

    clasefront

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//alert("hola mundo")
+const nameInput = document.getElementById('nameInput')
+const dateInput = document.getElementById('dateInput')
+const emailInput = document.getElementById('emailInput')
+const signupButton = document.getElementById('signupButton')
+
+
 let alfa = 10;
 let beta = "Juan Jose";
 let gamma = false;
+
+console.log(beta);
+
+//Eventos
+signupButton.addEventListener('click',signup);
+
+function signup(){
+    alert("CLICK!");
+}
