25d7c7469ef91444dbd2dad6843817ec4a277410
Domiciano Rincón <domiciano.rincon@correounivalle.edu.co>
2024-04-17 11:25:43 -0500

    Semana 11

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const emailInput = document.getElementById('emailInput');
 const signupButton = document.getElementById('signupButton');
 const coverImg = document.getElementById('coverImg');
 
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+
+const URL_BASE = "https://9bf8-200-3-193-78.ngrok-free.app";
+
 
 //alert("Hola mundo");
 
@@ -15,18 +20,31 @@ console.log(beta)
 
 //EVENTOS
 signupButton.addEventListener('click', signup);
+searchButton.addEventListener('click', search);
+
+
+async function search(){
+    let search = searchInput.value;
+    let response = await fetch(URL_BASE+'/user/searchByEmail/'+search);
+    if(response.status === 200){
+
+    }
+    else if(response.status === 400){
+        
+    }
+    let user = await response.json();
+    console.log(user);
+}
 
 function signup(){
     let name = nameInput.value;
     let password = passInput.value;
     let email = emailInput.value;
-
     let user = {
         name: name,
         password: password,
         email: email
     };
-
     postUser(user);
 }
 
@@ -34,10 +52,21 @@ async function postUser(user){
     //Obj a JSON
     let json = JSON.stringify(user);
     console.log(json);
+
+    let response = await fetch(URL_BASE+'/user/create',{
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: json
+    });
+
+    console.log(response);
+
 }
 
 async function getUsers(){
-    let response = await fetch("https://babc-200-3-193-78.ngrok-free.app/user/list"); //HTTP Requ
+    let response = await fetch(URL_BASE+"/user/list"); //HTTP Requ
     let users = await response.json();
     console.log(users);
 }
